32451fd87879c230f12cbcb27f34790d9db488ef
jorgeLQ <jorge.quilumba@epn.edu.ec>
2023-02-01 23:12:14 -0500

    version1.1
    -dash de candidaturas
    -diseño de dash
    -barra de progreso con porcentaje en cada polaridad
    -configuracion de porcentajes en graficas de barra horizontal y de pila

--- dash_application/candidatura_barrera.py
+++ dash_application/candidatura_barrera.py
@@ -5,10 +5,15 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import numpy as np
+import io
+import base64
 
 
-
-c1 = pd.read_excel(r'C:\Users\Jorge\Documents\tesis\static\archivos\candidatura_Barrera.xlsx')
+c1 = pd.read_excel('static/archivos/candidatura_Barrera.xlsx')
 c1b= "{:,}".format(c1.shape[0])
 
 #style
@@ -47,12 +52,19 @@ NAME_ALCALDE={
 'font-size': '19px'
 }
 BG_CONTENT={
-'background-color': '#e7e7e7'
+'background-color': '#d2d2d2'
 }
 #distribución por sentimiento
+value_counts2 = c1['Sentimental_Analysis'].value_counts()
+
+
 value_counts = c1['Sentimental_Analysis'].value_counts()
 num_P = value_counts['P']
 num_N = value_counts['N']
+
+num_Pc1= "{:,}".format(num_P)
+num_Nc1= "{:,}".format(num_N)
+
 num_P_percent = round(num_P / (num_P + num_N) * 100, 1)
 num_N_percent = round(num_N / (num_P + num_N) * 100, 1)
 num_P_percent_str = "{}%".format(num_P_percent)
@@ -68,8 +80,98 @@ def create_pie():
     return fig
 
 
+def create_hbar():
+    tendencia = c1[['Id','hashtags']]
+    tendencia = tendencia.dropna()
+    tendencia['hashtags'] = tendencia['hashtags'].str.split(',', expand=True)[0]
+    tendencia_count = tendencia['hashtags'].value_counts().head(5)
+    tendencia_count.to_frame()
+    tabla = pd.DataFrame(tendencia_count.to_frame())
+    tabla = tabla.rename(columns ={'hashtags':'Count'})
+    tendencia_count = tabla.sort_values(by='Count', ascending=True)
+    tendencia_count = tendencia_count.reset_index()
+    tendencia_count = tendencia_count.rename(columns ={'index':'hashtags'})
+    fig = go.Figure(data=[
+        go.Bar(
+            name='Tendencia',
+            y=tendencia_count['hashtags'],
+            x=tendencia_count['Count'],
+            orientation='h',
+            text=tendencia_count['Count'],
+            textposition='inside',
+        )
+    ])
+
+    fig.update_layout(title='Top de Tendencias', title_x=0.5,font=dict(size=18))
+    return fig
+
+
+
+
+
+def create_bar_polarity():
+    df_pivot = c1.pivot_table(index=[c1['Date'].dt.year, 'Id'], columns='Sentimental_Analysis', aggfunc='size', fill_value=0)
+    df_grouped = df_pivot.groupby(level=0).sum()
+
+    trace1 = go.Bar(x=df_grouped.index, y=df_grouped['P'], name='P', marker=dict(color='#2bc58b'),text=df_grouped['P'].astype(str))
+    trace2 = go.Bar(x=df_grouped.index, y=df_grouped['N'], name='N', marker=dict(color='#f44d4d'),text=df_grouped['N'].astype(str))
+    data = [trace1, trace2]
+    layout = go.Layout(title='Análisis de Sentimiento por Año', title_x=0.5, font=dict(size=18),barmode='stack', xaxis=dict(title='Año'), yaxis=dict(title='Cantidad'))
+    fig = go.Figure(data=data, layout=layout)
+    return fig
+
+
+
+def create_topics():
+    df = c1
+    df_grouped = df.groupby(['topic_name','Sentimental_Analysis']).size().reset_index(name='counts')
+    df_grouped
+    df_grouped = df.groupby(['topic_name','Sentimental_Analysis']).size().reset_index(name='counts')
+    def add_percentage(val):
+        return f"{val:.2f}%"
+    df_grouped = df_grouped.pivot(index='topic_name', columns='Sentimental_Analysis', values='counts')
+    df_grouped['Total'] = df_grouped.sum(axis=1)
+    df_grouped['P_percent'] = round(df_grouped['P'] / df_grouped['Total'] * 100, 2)
+    df_grouped['N_percent'] = round(df_grouped['N'] / df_grouped['Total'] * 100, 2)
+
+
+    fig = go.Figure(data=[
+        go.Bar(
+            name='Positivo',
+            y=df_grouped.index.tolist(),
+            x=df_grouped['P_percent'],
+            orientation='h',
+            text=df_grouped['P_percent'].astype(str) + '%',
+            textposition='outside',
+            marker=dict(color='#2bc58b')
+        ),
+        go.Bar(
+            name='Negativo',
+            y=df_grouped.index.tolist(),
+            x=df_grouped['N_percent'],
+            orientation='h',
+            text=df_grouped['N_percent'].astype(str) + '%',
+            textposition='outside',
+            marker=dict(color='#f44d4d')
+        )
+    ])
+
+    fig.update_layout(
+        title='Análisis de Sentimiento por tópico', title_x=0.5,font=dict(size=18), height=600,
+        yaxis=dict(
+            tickmode="array",
+            tickvals=df_grouped.index.tolist(),
+            ticktext=df_grouped.index.tolist(),
+            side='left'
+        )
+    )
+
+    return fig
+
+
 def create_dash_application(flask_app):
     dash_app = dash.Dash(server=flask_app, name="Dashboard", url_base_pathname="/augustobarrera/",external_stylesheets=[dbc.themes.BOOTSTRAP])
+
     dash_app.layout = html.Div(
         children=[
                 html.Div([
@@ -85,68 +187,26 @@ def create_dash_application(flask_app):
 
                 html.Br(),
 
-
-                        html.Div([
-                            html.Div([
-                                html.Div([
-                                    html.Div([
-                                        html.Img(src=('../static/images/Barrera.png')),
-                                        html.P("Augusto Barrera",style=NAME_ALCALDE),
-                                        html.P("31/07/2009 - 14/05/2014"),
-                                    ], className='bg-white card'),
-
-                                ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4 ',style={'text-align': 'center'}),
-
-
-                                html.Div([
-
-                                    html.Div([
-                                        html.Div([
-                                            html.Div([
-                                                html.Img(src=('../static/images/twitter.png')),
-                                                html.P("Tweets Totales"),
-                                                html.P(c1b),
-                                            ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4'),
-
-                                            html.Div([
-                                                html.Img(src=('../static/images/smile.png')),
-                                                html.P("Positivos"),
-                                                html.P(num_P),
-                                            ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4'),
-
-                                            html.Div([
-                                                html.Img(src=('../static/images/sad-face.png')),
-                                                html.P("Negativos"),
-                                                html.P(num_N),
-                                            ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4'),
-
-                                        ],className='row d-flex'),
-                                    ],className='container', style={'padding-top': '40%'}),
-
-                                ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4 card',style={'text-align': 'center'}),
-
-                                html.Div([
-                                    html.Div([
-                                        dcc.Graph(figure=create_pie()),
-                                    ], className='bg-white card'),
-                                ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4',style={'text-align': 'center'}),
-
-                            ],className='row justify-content-between'),
-                        ],className='container'),
-
-                html.Br(),html.Br(),html.Br(),
-
                 html.Div([
 
                     html.Div([
                         html.Div([
                             html.Div([
-                                html.Img(src=('../static/images/Barrera.png')),
-                                html.P("Augusto Barrera",style=NAME_ALCALDE),
-                                html.P("31/07/2009 - 14/05/2014"),
+                                html.Div([
+                                    html.Div([
+                                        html.Div([
+                                            html.Img(src=('../static/images/Barrera.png'),style={'width': '90px'}),
 
-                            ],className='card-body',style={'padding-top':'20%'})
-                        ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4 card',style={'text-align': 'center'}),
+                                        ],className='col-xl-6 col-md-6 mb-6'),
+                                        html.Div([
+                                            html.Span(" Augusto Barrera ",style=NAME_ALCALDE),
+                                            html.Span("31/07/2009 - 14/05/2014"),
+
+                                        ],className='col-xl-6 col-md-6 mb-6'),
+                                    ],className='row no-gutters align-items-center text-center'),
+                                ],className='card-body'),
+                            ],className='card shadow h-100 py-2'),
+                        ],className='col-xl-3 col-md-6 mb-3'),
 
                         html.Div([
                             html.Div([
@@ -154,39 +214,115 @@ def create_dash_application(flask_app):
                                     html.Div([
                                         html.Div([
                                             html.Img(src=('../static/images/twitter.png')),
-                                            html.P("Tweets Totales"),
-                                            html.P(c1b),
-                                        ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4'),
-
+                                        ],className='col-xl-6 col-md-6 mb-6'),
                                         html.Div([
-                                            html.Img(src=('../static/images/smile.png')),
-                                            html.P("Positivos"),
-                                            html.P(num_P),
-                                        ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4'),
+                                            html.Span(" Total Tweets ",style=NAME_ALCALDE),
+                                            html.Span(c1b, style={'font-size': '30px'}),
+                                        ],className='col-xl-6 col-md-6 mb-6'),
 
-                                        html.Div([
-                                            html.Img(src=('../static/images/sad-face.png')),
-                                            html.P("Negativos"),
-                                            html.P(num_N),
-                                        ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4'),
+                                    ],className='row no-gutters text-center'),
 
-                                    ],className='row'),
-                                ],className='container',style={'padding-top': '30%'}),
-
-                            ],className='card-body')
-                        ],className=' col-xs-12 col-sm-6 col-md-4 col-lg-4 card',style={'text-align': 'center'}),
+                                ],className='card-body'),
+                            ],className='card shadow h-100 py-2'),
+                        ],className='col-xl-3 col-md-6 mb-3'),
 
                         html.Div([
                             html.Div([
-                                dcc.Graph(figure=create_pie()),
-                            ],className='card-body')
-                        ],className='col-xs-12 col-sm-6 col-md-4 col-lg-4 card',style={'text-align': 'center'}),
+                                html.Div([
+                                    html.Div([
+                                        html.Div([
+                                            html.Img(src=('../static/images/smile.png')),
+                                        ],className='col-xl-6 col-md-6 mb-6'),
+                                        html.Div([
+                                            html.Span(" Total Positivos ",style=NAME_ALCALDE),
+
+                                            html.Span(num_Pc1, style={'font-size': '30px'}),
+                                            dbc.Progress(num_P_percent_str, value=num_P_percent, color="#2bc58b", style={'height': '35px','font-size': '20px'}),
+                                        ],className='col-xl-6 col-md-6 mb-6'),
+                                    ],className='row no-gutters text-center'),
+
+                                ],className='card-body'),
+                            ],className='card shadow h-100 py-2'),
+                        ],className='col-xl-3 col-md-6 mb-3'),
+
+                        html.Div([
+                            html.Div([
+                                html.Div([
+                                    html.Div([
+                                        html.Div([
+                                            html.Img(src=('../static/images/sad-face.png')),
+                                        ],className='col-xl-6 col-md-6 mb-6'),
+                                        html.Div([
+                                            html.Span(" Total Negativos ",style=NAME_ALCALDE),
+                                            html.Span(num_Nc1, style={'font-size': '30px'}),
+                                            dbc.Progress(num_N_percent_str, value=num_N_percent, color="#f44d4d", style={'height': '35px','font-size': '20px'}),
+                                        ],className='col-xl-6 col-md-6 mb-6'),
+                                    ],className='row no-gutters text-center'),
+                                ],className='card-body'),
+                            ],className='card shadow h-100 py-2'),
+                        ],className='col-xl-3 col-md-6 mb-3'),
+
+                    ],className='row'),
 
 
-                    ],className='row')
-                ],className='container '),
+                    html.Div([
+                        html.Div([
+                            html.Div([
+                                html.Div([
+                                    html.Div([
+                                        dcc.Graph(figure=create_topics(),style={'width': '100%'}),
+                                    ],className='row no-gutters align-items-center text-center'),
+                                ]),
+                            ],className='card border-left-success shadow h-100 py-2'),
+                        ],className='col-xl-12 col-md-12 mb-12'),
+
+                    ],className='row'),
+                    html.Br(),
+
+                    html.Div([
+                        html.Div([
+                            html.Div([
+                                html.Div([
+                                    html.Div([
+                                        dcc.Graph(figure=create_hbar(),style={'width': '100%'}),
+
+                                    ],className='row no-gutters text-center'),
+                                ]),
+                            ],className='card shadow py-2'),
+                        ],className='col-xl-4 col-md-6 mb-4'),
+
+                        html.Div([
+                            html.Div([
+                                html.Div([
+                                    html.Div([
+
+                                        dcc.Graph(figure=create_bar_polarity(),style={'width': '100%'}),
+
+                                    ],className='row no-gutters text-center'),
+                                ]),
+                            ],className='card  shadow py-2'),
+                        ],className='col-xl-5 col-md-6 mb-5'),
+
+                        html.Div([
+                            html.Div([
+                                html.Div([
+                                    html.Div([
+                                        html.P('Nube de palabras',style={'font-size':'25px','color': '#304463','padding-left': '26%'}),
+                                        html.Img(src=('../static/images/word_cloud_c1.png'),style={'width': '100%','height': '365px'}),
+                                    ],className='row no-gutters text-center'),
+                                ],className='card-body'),
+                            ],className='card shadow py-2'),
+                        ],className='col-xl-3 col-md-6 mb-3'),
+
+                    ],className='row'),
+
+
+                ],className='container-fluid'),
 
                 html.Br(),html.Br(),html.Br(),
+
+
+
         ],style=BG_CONTENT)
 
 
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, make_response, redirect, render_template
 from dash_application.candidatura_barrera import *
+from dash_application.candidatura_rodas import *
+from dash_application.candidatura_yunda import *
+from dash_application.candidatura_guarderas import *
 import pandas as pd
 import plotly.graph_objects as go
 
-c1 = pd.read_excel(r'C:\Users\Jorge\Documents\tesis\static\archivos\candidatura_Barrera.xlsx')
+c1 = pd.read_excel('static/archivos/candidatura_Barrera.xlsx')
+c2 = pd.read_excel('static/archivos/candidatura_Rodas.xlsx')
+c3 = pd.read_excel('static/archivos/candidatura_Yunda.xlsx')
+c4 = pd.read_excel('static/archivos/candidatura_Guarderas.xlsx')
+
 app =Flask(__name__)
 create_dash_application(app)
+create_dash_application2(app)
+create_dash_application3(app)
+create_dash_application4(app)
 
 @app.route('/')
 def home():
@@ -20,10 +30,52 @@ def home():
     num_P_percent_str = "{}%".format(num_P_percent)
     num_N_percent_str = "{}%".format(num_N_percent)
 
+    c2b= "{:,}".format(c2.shape[0])
+    '''Sentimiento por candidatura'''
+    value_counts2 = c2['Sentimental_Analysis'].value_counts()
+    num_P2 = value_counts2['P']
+    num_N2 = value_counts2['N']
+    num_P_percent2 = round(num_P2 / (num_P2 + num_N2) * 100, 1)
+    num_N_percent2 = round(num_N2 / (num_P2 + num_N2) * 100, 1)
+    num_P_percent_str2 = "{}%".format(num_P_percent2)
+    num_N_percent_str2 = "{}%".format(num_N_percent2)
+
+    c3b= "{:,}".format(c3.shape[0])
+    '''Sentimiento por candidatura'''
+    value_counts3 = c3['Sentimental_Analysis'].value_counts()
+    num_P3 = value_counts3['P']
+    num_N3 = value_counts3['N']
+    num_P_percent3 = round(num_P3 / (num_P3 + num_N3) * 100, 1)
+    num_N_percent3 = round(num_N3 / (num_P3 + num_N3) * 100, 1)
+    num_P_percent_str3 = "{}%".format(num_P_percent3)
+    num_N_percent_str3 = "{}%".format(num_N_percent3)
+
+    c4b= "{:,}".format(c4.shape[0])
+    '''Sentimiento por candidatura'''
+    value_counts4 = c4['Sentimental_Analysis'].value_counts()
+    num_P4 = value_counts4['P']
+    num_N4 = value_counts4['N']
+    num_P_percent4 = round(num_P4 / (num_P4 + num_N4) * 100, 1)
+    num_N_percent4 = round(num_N4 / (num_P4 + num_N4) * 100, 1)
+    num_P_percent_str4 = "{}%".format(num_P_percent4)
+    num_N_percent_str4 = "{}%".format(num_N_percent4)
+
     context={
         'c1b':c1b,
         'num_P_percent_str':num_P_percent_str,
-        'num_N_percent_str':num_N_percent_str
+        'num_N_percent_str':num_N_percent_str,
+
+        'c2b':c2b,
+        'num_P_percent_str2':num_P_percent_str2,
+        'num_N_percent_str2':num_N_percent_str2,
+
+        'c3b':c3b,
+        'num_P_percent_str3':num_P_percent_str3,
+        'num_N_percent_str3':num_N_percent_str3,
+
+        'c4b':c4b,
+        'num_P_percent_str4':num_P_percent_str4,
+        'num_N_percent_str4':num_N_percent_str4
     }
 
     return render_template('home.html', **context)
